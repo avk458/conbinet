58b2fded59d3f50df3e021b67b4be657ae0219d4
kano <avk458@gmail.com>
2020-08-23 22:15:16 +0800

    info modal add axios...

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,25 +1,91 @@
 <template>
   <div id="app">
-    <a-row :gutter="8">
-      <a-col :span="2"/>
-      <a-col :md="6" :lg="10">
-        <ObjectCard title="人员简介"/>
-      </a-col>
-      <a-col :md="6" :lg="10">
-        <ObjectCard title="业绩展示"/>
-      </a-col>
-      <a-col :span="2"/>
-    </a-row>
+    <div v-if="!isLogin" class="login-div">
+      <a-form layout="horizontal" :form="form" @submit="handleSubmit">
+        <a-form-item :validate-status="userNameError() ? 'error' : ''" :help="userNameError() || ''">
+          <a-input
+              v-decorator="[
+          'username',
+          { rules: [{ required: true, message: 'Please input your username!' }] },
+        ]"
+              placeholder="Username"
+          >
+            <a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item :validate-status="passwordError() ? 'error' : ''" :help="passwordError() || ''">
+          <a-input
+              v-decorator="[
+          'password',
+          { rules: [{ required: true, message: 'Please input your Password!' }] },
+        ]"
+              type="password"
+              placeholder="Password"
+          >
+            <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" html-type="submit" :disabled="hasErrors(form.getFieldsError())">
+            Log in
+          </a-button>
+        </a-form-item>
+      </a-form>
+    </div>
+    <div v-else>
+      <a-row :gutter="8">
+        <a-col :span="2"/>
+        <a-col :md="6" :lg="10">
+          <ObjectCard title="人员简介" type="person"/>
+        </a-col>
+        <a-col :md="6" :lg="10">
+          <ObjectCard title="业绩展示" type="performance"/>
+        </a-col>
+        <a-col :span="2"/>
+      </a-row>
+    </div>
   </div>
 </template>
 
 <script>
 import ObjectCard from './components/ObjectCard.vue'
-
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
 export default {
   name: 'App',
   components: {
     ObjectCard
+  },
+  data() {
+    return {
+      isLogin: false,
+      hasErrors,
+      form: this.$form.createForm(this, { name: 'horizontal_login' }),
+    }
+  },
+  methods: {
+    // Only show error after a field is touched.
+    userNameError() {
+      const { getFieldError, isFieldTouched } = this.form;
+      return isFieldTouched('userName') && getFieldError('userName');
+    },
+    // Only show error after a field is touched.
+    passwordError() {
+      const { getFieldError, isFieldTouched } = this.form;
+      return isFieldTouched('password') && getFieldError('password');
+    },
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          const { username, password } = values
+          if ('admin' === username && '999999' === password) {
+            this.isLogin = true
+          }
+        }
+      });
+    },
   }
 }
 </script>
@@ -27,5 +93,9 @@ export default {
 <style>
 #app{
   margin: 100px 0 0 0;
+}
+.login-div{
+  margin: auto;
+  width: 400px;
 }
 </style>
---
+++ web/src/components/AddModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <a-modal v-model="visible" :title="title" @ok="handleOk">
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+      <p>Some contents...</p>
+    </a-modal>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+      title: ''
+    };
+  },
+  methods: {
+    showModal(type) {
+      switch (type) {
+        case 'person':
+          this.title = '新增人员简介信息'
+              break
+        case 'performance':
+          this.title = '新增业绩展示信息'
+              break
+      }
+      this.visible = true;
+    },
+    handleOk(e) {
+      console.log(e);
+      this.visible = false;
+    },
+  },
+};
+</script>
